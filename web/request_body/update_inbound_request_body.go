3b6aa51fb8e8d9c4b3fec72869cc42465550a41c
amir <ba97140@gmail.com>
2022-11-19 09:01:05 +0330

    added UpdateInboundRequestBody

---
+++ web/request_body/update_inbound_request_body.go
@@ -0,0 +1,14 @@
+package request_body
+
+type UpdateInboundRequestBody struct {
+	Total          int64  `json:"total" binding:"number"`
+	Remark         string `json:"remark"`
+	Enable         bool   `json:"enable"`
+	ExpiryTime     int64  `json:"expiry_time"`
+	Listen         string `json:"listen"`
+	Port           int    `json:"port" binding:"required,gt=0,lt=65536"`
+	Protocol       string `json:"protocol" binding:"required,oneof=vmess vless trojan shadowsocks socks http dokodemo-door"`
+	Settings       string `json:"settings" binding:"base64"`
+	StreamSettings string `json:"stream_settings" binding:"base64"`
+	Sniffing       string `json:"sniffing" binding:"base64"`
+}
